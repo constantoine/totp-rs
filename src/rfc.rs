fe2363c77bb05a480b9d836a3ac47e4a714a7362
Steven Salaun <steven.salaun@gmail.com>
2022-08-08 17:30:27 +0200

    TOTP::new checks digits value compliance to rfc

--- src/rfc.rs
+++ src/rfc.rs
@@ -29,7 +29,7 @@ impl std::fmt::Display for Rfc6238Error {
     }
 }
 
-fn assert_digits(digits: &usize) -> Result<(), Rfc6238Error> {
+pub fn assert_digits(digits: &usize) -> Result<(), Rfc6238Error> {
     if !(&6..=&8).contains(&digits) {
         Err(Rfc6238Error::InvalidDigits)
     } else {
